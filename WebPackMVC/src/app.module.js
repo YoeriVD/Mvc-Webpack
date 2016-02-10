b2c6f6dbd0a97cda5a9a63a9c03f9fd5a89e0dc1
YoeriVD <yoeri.vandamme@gmail.com>
2016-02-10 16:14:10 +0100

    clean up

--- WebPackMVC/src/app.module.js
+++
@@ -1,7 +0,0 @@
-/// <reference path="typings/tsd.d.ts" />
-var angular = require("angular");
-var main_controller_1 = require("./main/main.controller");
-angular
-    .module('sd', [])
-    .controller("MainController", main_controller_1.MainController);
-//# sourceMappingURL=app.module.js.map
